e17d3f96a3f9b2317479bdc4f899c7e769084da1
theforps <theforps@gmail.com>
2022-09-22 12:29:00 +0300

    Добавьте файлы проекта.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Randomize.Models;
+
+namespace Randomize.Controllers
+{
+    public class MainController : Controller
+    {
+        //get
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Main(Digits digit)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Digits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomize.Models
+{
+    public class Digits
+    {
+        
+        int numberOne { get; set; }
+        int numberTwo { get; set; }
+
+        int result { get; set; }
+    }
+}
