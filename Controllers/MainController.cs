a570ef62a5ff38744b4762e8ae8c206dc9771f01
theforps <theforps@gmail.com>
2022-09-25 21:07:10 +0300

    Update 25/09/22

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Randomize.Models;
 
 namespace Randomize.Controllers
--- Models/Digits.cs
+++ Models/Digits.cs
@@ -6,7 +6,8 @@ namespace Randomize.Models
     {
         [Required (ErrorMessage = "Введите число в диапазоне от -10000 до 10000")]
         public int numberOne { get; set; }
-        [Required(ErrorMessage = "Введите число в диапазоне от -10000 до 10000")]
+
+        [Required (ErrorMessage = "Введите число в диапазоне от -10000 до 10000")]
         public int numberTwo { get; set; }
 
         public int result { get; set; }
