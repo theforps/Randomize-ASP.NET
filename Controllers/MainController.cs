e2dc62b38eda1bb0603163b11c0efa8433f9970e
theforps <theforps@gmail.com>
2022-09-26 21:01:04 +0300

    Update 26/09/22

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -16,6 +16,11 @@ namespace Randomize.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Main(Digits digit)
         {
+            if (digit.numberTwo == digit.numberOne)
+                ModelState.AddModelError("numberOne", "The numbers are equal");
+            else if (digit.numberTwo > digit.numberOne)
+                ModelState.AddModelError("numberOne", "Incorrectly entered numbers in the range");
+
             if (ModelState.IsValid)
             {
                 Random rand = new Random();
--- Models/Digits.cs
+++ Models/Digits.cs
@@ -4,10 +4,10 @@ namespace Randomize.Models
 {
     public class Digits
     {
-        [Required (ErrorMessage = "Введите число в диапазоне от -10000 до 10000")]
+        [Required (ErrorMessage = "Enter a number in the range from -10000 to 10000")]
         public int numberOne { get; set; }
 
-        [Required (ErrorMessage = "Введите число в диапазоне от -10000 до 10000")]
+        [Required (ErrorMessage = "Enter a number in the range from -10000 to 10000")]
         public int numberTwo { get; set; }
 
         public int result { get; set; }
